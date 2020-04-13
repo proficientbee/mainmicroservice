a611d771136cbf7c963293bf0ce949b16cbc3669
profi <proficientbee@gmail.com>
2020-04-13 13:39:32 +0530

    Initiated the Spring Boot Project

---
+++ src/test/java/com/proficientbee/mainmicroservice/MainmicroserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.proficientbee.mainmicroservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MainmicroserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
